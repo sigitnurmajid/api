5cc60f8035e06f45658add98ac54de514852a145
sigitnurmajid <sigitnurmajid32@gmail.com>
2021-09-23 09:42:38 +0700

    add basic auth

--- app.ts
+++ app.ts
@@ -1,11 +1,21 @@
 import express, { Request, Response, NextFunction, request } from 'express'
 import MoController from './controller/MoController'
 import bodyParser from 'body-parser'
+import basicAuth from 'express-basic-auth'
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser())
+
+app.use(basicAuth({authorizer : myAuthorizer}))
+ 
+function myAuthorizer(username : string, password : string) {
+    const userMatches = basicAuth.safeCompare(username, 'it')
+    const passwordMatches = basicAuth.safeCompare(password, 'admin')
+ 
+    return userMatches && passwordMatches
+}
 
 app.listen(port, () => {
     console.log(`Your application is running on port ${port}.`);
